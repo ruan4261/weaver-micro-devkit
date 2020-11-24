0dc5525c7220537fac1eb7764a0fdaa289d7013b
ruan4261 <i@4261.ink>
2020-11-25 00:53:47 +0800

    [代码完善](v0.2.0): WorkflowAPI中关于bill表的问题修复，向后兼容，不影响使用，请查看JavaDoc

--- src/main/java/weaver/micro/devkit/api/WorkflowAPI.java
+++ src/main/java/weaver/micro/devkit/api/WorkflowAPI.java
@@ -16,7 +16,7 @@ import java.util.*;
 public final class WorkflowAPI {
     static final String EMPTY = "";
     /** 流程中的签字意见类型映射 */
-    static final Map<String, String> logTypeMapper = new HashMap<String, String>() {
+    static final Map<String, String> LOG_TYPE_MAPPER = new HashMap<String, String>() {
         public Map<String, String> construct() {
             super.put("0", "批准");
             super.put("1", "保存");
@@ -53,7 +53,7 @@ public final class WorkflowAPI {
      *         nodeid 节点id
      *         nodename 节点名称
      *         operator 操作者id，如为1则是系统管理员，此id无法查询人力资源表
-     *         logtype 签字意见，已对应转化为确定类型，详情请查看{@link #logTypeMapper}
+     *         logtype 签字意见，已对应转化为确定类型，详情请查看{@link #LOG_TYPE_MAPPER}
      *         remark 签字意见
      *         operatedate 操作日期
      *         operatetime 操作时间
@@ -72,7 +72,7 @@ public final class WorkflowAPI {
             map.put("nodeid", Util.null2String(rs.getString("nodeid")));// 节点id
             map.put("nodename", Util.null2String(rs.getString("nodename")));// 节点名称
             map.put("operator", Util.null2String(rs.getString("operator")));// 操作者的id
-            map.put("logtype", Util.null2String(logTypeMapper.get(rs.getString("logtype"))));// 签字类型
+            map.put("logtype", Util.null2String(LOG_TYPE_MAPPER.get(rs.getString("logtype"))));// 签字类型
             map.put("remark", Util.delHtml(Util.null2String(rs.getString("remark"))));// 签字意见
             map.put("operatedate", Util.null2String(rs.getString("operatedate")));// 操作日期
             map.put("operatetime", Util.null2String(rs.getString("operatetime")));// 操作时间
@@ -85,20 +85,21 @@ public final class WorkflowAPI {
      * 获取表单字段简写到id的映射
      * 只可获取表单名称为formtable_main_{billid}的表单及其明细表_dt{tableIdx}字段映射
      *
-     * @param billid   流程表单id
-     * @param tableIdx 0：主表，大于0为明细表
+     * @param billId  流程表单id
+     * @param orderId 0：主表，大于0为明细表
      * @return 字段映射
      */
-    public static Map<String, String> queryFieldMapper(int billid, final int tableIdx) {
+    public static Map<String, String> queryFieldMapper(int billId, int orderId) {
         Map<String, String> result = new HashMap<String, String>();
-        billid = Math.abs(billid);
         RecordSet rs = new RecordSet();
 
-        String sql = "select id,fieldname from workflow_billfield where billid=-" + billid;
-        if (tableIdx == 0)
+        String sql = "select id,fieldname from workflow_billfield where billid=-" + billId;
+        if (orderId == 0)
             sql += " and (detailtable is null or detailtable = '') ";
-        else
-            sql += " and detailtable='formtable_main_" + billid + "_dt" + tableIdx + "'";
+        else {
+            String detailTableName = getDetailTableNameByBillIdAndOrderId(billId, orderId);
+            sql += " and detailtable='" + detailTableName + "'";
+        }
 
         rs.execute(sql);
         while (rs.next()) {
@@ -108,9 +109,20 @@ public final class WorkflowAPI {
         return result;
     }
 
+    @Deprecated
     public static int getBillId(int workflowId) {
+        return getBillIdByWorkflowId(workflowId);
+    }
+
+    public static int getBillIdByWorkflowId(int workflowId) {
         String sql = "select b.id from workflow_base a,workflow_bill b where a.formid=b.id and a.id =" + workflowId;
         return Cast.o2Integer(CommonAPI.querySingleField(sql, "id"));
+    }
+
+    /** @param workflowId 流程id */
+    @Deprecated
+    public static String getBillTableName(int workflowId) {
+        return getBillTableNameByWorkflowId(workflowId);
     }
 
     /**
@@ -119,7 +131,7 @@ public final class WorkflowAPI {
      * @param workflowId 流程id
      * @return 数据库表单名称
      */
-    public static String getBillTableName(int workflowId) {
+    public static String getBillTableNameByWorkflowId(int workflowId) {
         String sql = "select b.tablename from workflow_base a,workflow_bill b where a.formid=b.id and a.id=" + workflowId;
         return CommonAPI.querySingleField(sql, "tablename");
     }
@@ -189,7 +201,8 @@ public final class WorkflowAPI {
 
     /**
      * 查询某条请求的主表信息
-     * 不知道数据库表单可以使用{@link #queryBillTableByRequest(int)}方法
+     * 不知道数据库表单可以使用{@link #queryBillTableByRequest(int)}方法，
+     * 推荐使用{@link #getBillTableNameByBillId(int)}或{@link #getBillTableNameByWorkflowId(int)}
      *
      * @param billTableName 数据库表单，必须为formtable_main_{num}格式
      *                      选择明细表请使用{@link #queryRequestDetailData(String, int, int)}方法
@@ -207,27 +220,39 @@ public final class WorkflowAPI {
     }
 
     /**
+     * @param billId    主表billId
+     * @param requestId 请求标识
+     */
+    public static Map<String, String> getRequestMainTableData(int billId, int requestId) {
+        String billTableName = getBillTableNameByBillId(billId);
+        return queryRequestMainData(billTableName, requestId);
+    }
+
+    /**
      * 查询某条请求的明细表信息
      * 不知道数据库表单可以使用{@link #queryBillTableByRequest(int)}方法
      *
      * @param billTableName 数据库表单，必须为formtable_main_{num}格式，无需写dt参数
      * @param requestId     流程的请求id
-     * @param table         明细表从1开始，实际上就是表名_dt后面的数
+     * @param orderId       明细表序号
      * @return 多行明细映射信息
      */
-    public static List<Map<String, String>> queryRequestDetailData(final String billTableName, final int requestId, final int table) {
+    public static List<Map<String, String>> queryRequestDetailData(String billTableName, int requestId, int orderId) {
         Assert.notEmpty(billTableName);
         List<Map<String, String>> result = new ArrayList<Map<String, String>>();
         RecordSet rs = new RecordSet();
 
-        // 获取主表id，用于明细表关联
+        // 通过billTableName及requestId获取mainid，用于明细表关联
         String sql = "select id from " + billTableName + " where requestid = '" + requestId + "'";
         rs.execute(sql);
         if (!rs.next()) return result;
         int mainid = Util.getIntValue(rs.getString("id"));
 
+        // 获取明细表名
+        String detailTableName = getDetailTableNameByBillTableNameAndOrderId(billTableName, orderId);
+
         // 查询明细表
-        sql = "select * from " + billTableName + "_dt" + table + " where mainid= '" + mainid + "'";
+        sql = "select * from " + detailTableName + " where mainid= '" + mainid + "'";
         rs.execute(sql);
         while (rs.next()) {
             result.add(CommonAPI.mapFromRecordRow(rs));
@@ -236,10 +261,20 @@ public final class WorkflowAPI {
     }
 
     /**
+     * @param billId    主表billId
+     * @param requestId 请求标识
+     * @param orderId   表序号，0未主表，其余为明细表序号
+     */
+    public static List<Map<String, String>> getRequestDetailData(int billId, int requestId, int orderId) {
+        String billTableName = getBillTableNameByBillId(billId);
+        return queryRequestDetailData(billTableName, requestId, orderId);
+    }
+
+    /**
      * 根据logid获取节点名称
      */
-    public static String getNodeNameByLogId(int logid) {
-        int nodeid = Util.getIntValue(CommonAPI.querySingleField("select nodeid from workflow_requestlog where logid=" + logid, "nodeid"));
+    public static String getNodeNameByLogId(int logId) {
+        int nodeid = Util.getIntValue(CommonAPI.querySingleField("select nodeid from workflow_requestlog where logid=" + logId, "nodeid"));
         if (nodeid == -1) return EMPTY;
         return CommonAPI.querySingleField("select nodename from workflow_nodebase where id=" + nodeid, "nodename");
     }
@@ -247,26 +282,26 @@ public final class WorkflowAPI {
     /**
      * 获取表单某字段的id
      *
-     * @param billId   主表billId
-     * @param tableIdx 0代表主表，其余代表明细表
-     * @param name     表单字段数据库名
+     * @param billId  主表billId
+     * @param orderId 0代表主表，其余代表明细表
+     * @param name    表单字段数据库名
      * @return 表单字段id
      */
-    public static int getFieldIdByFieldName(int billId, int tableIdx, String name) {
-        billId = Math.abs(billId);
+    public static int getFieldIdByFieldName(int billId, int orderId, String name) {
         RecordSet rs = new RecordSet();
 
-        String sql = "select id from workflow_billfield where fieldname='" + name + "' and billid=-" + billId;
-        if (tableIdx == 0)
+        String sql = "select id from workflow_billfield where fieldname='" + name + "' and billid=" + billId;
+        if (orderId == 0)
             sql += " and (detailtable is null or detailtable = '') ";
-        else
-            sql += " and detailtable='formtable_main_" + billId + "_dt" + tableIdx + "'";
+        else {
+            String detailTableName = getDetailTableNameByBillIdAndOrderId(billId, orderId);
+            sql += " and detailtable='" + detailTableName + "'";
+        }
 
         rs.execute(sql);
-        if (!rs.next()) {
-            String tablename = "formtable_main_" + billId + (tableIdx == 0 ? "" : ("_dt" + tableIdx));
-            throw new RuntimeException("No such field, input : billId=" + billId + ", billTable=" + tablename + ", field(notExist)=" + name);
-        }
+        if (!rs.next())
+            throw new RuntimeException("No such field, input : billId=" + billId + ", orderId=" + orderId + ", field(notExist)=" + name);
+
         return rs.getInt("id");
     }
 
@@ -281,6 +316,32 @@ public final class WorkflowAPI {
         rs.execute("select selectvalue,selectname from workflow_selectitem where fieldid=" + fieldId + " and selectvalue=" + valueIdx);
         if (!rs.next())
             throw new RuntimeException("No such select item, input : fieldId=" + fieldId + ", selectvalue=" + valueIdx);
+
         return rs.getString("selectname");
+    }
+
+    public static String getBillTableNameByBillId(int billId) {
+        String sql = "select tablename from workflow_bill where id=" + billId;
+        return CommonAPI.querySingleField(sql, "tablename");
+    }
+
+    public static int getBillIdByBillTableName(String billTableName) {
+        String sql = "select id from workflow_bill where tablename='" + billTableName + "'";
+        return Cast.o2Integer(CommonAPI.querySingleField(sql, "id"));
+    }
+
+    /**
+     * @param billId  主表billid
+     * @param orderId 明细表序号，为0时取主表名
+     */
+    public static String getDetailTableNameByBillIdAndOrderId(int billId, int orderId) {
+        if (orderId == 0) return getBillTableNameByBillId(billId);
+        return CommonAPI.querySingleField("select tablename from workflow_billdetailtable where billid=" + billId + " and orderid=" + orderId, "tablename");
+    }
+
+    public static String getDetailTableNameByBillTableNameAndOrderId(String billTableName, int orderId) {
+        if (orderId == 0) return billTableName;
+        int billId = getBillIdByBillTableName(billTableName);
+        return getDetailTableNameByBillIdAndOrderId(billId, orderId);
     }
 }
