a5ac7c1256c67d4016bf44665e1e0bfbb7d0e801
ruan4261 <i@4261.ink>
2021-03-03 18:31:14 +0800

    :building_construction: important(1.1.0): 不兼容的修改 | Incompatible modifications
    fix: CommonHttpAPI重大问题, 原接口中仅可读返回报文头, 由于连接过早关闭导致无法读取报文主体.
    incompatible: CommonHttpAPI中的原请求方法入参已将HttpClient改为CloseableHttpClient, 返回值改为CloseableHttpResponse
    feature: 新增httpResponse处理类HttpResponseHolder, CommonHttpAPI中已添加返回值包装方法

--- src/main/java/weaver/micro/devkit/io/HttpAPI.java
+++ src/main/java/weaver/micro/devkit/io/HttpAPI.java
@@ -1,15 +1,12 @@
 package weaver.micro.devkit.io;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.impl.client.CloseableHttpClient;
 import weaver.micro.devkit.Assert;
 import weaver.micro.devkit.http.CommonHttpAPI;
+import weaver.micro.devkit.http.HttpResponseHolder;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 /**
  * 仅允许http或https协议
@@ -17,7 +14,7 @@ import java.nio.charset.Charset;
 public class HttpAPI {
 
     public static void httpVerify(final String path) {
-        Assert.judge(new Assert.Judgement() {
+        Assert.judge(new Assert.Judgement<RuntimeException>() {
             @Override
             public boolean through() {
                 return path.startsWith("https://") || path.startsWith("http://");
@@ -27,50 +24,39 @@ public class HttpAPI {
 
     public static byte[] getRemoteBytes(String path) throws IOException {
         httpVerify(path);
-        HttpURLConnection conn = null;
-
-        try {
-            URL url = new URL(path);
-            // cast to HttpUrlConnection
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            // settings
-            conn.setConnectTimeout(15000);
-            conn.setReadTimeout(60000);
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36");
-            // open
-            conn.connect();
-            //通过conn取得输入流，并使用Reader读取
-            if (200 == conn.getResponseCode()) {
-                return IOAPI.getByteStreamData(conn.getInputStream());
-            } else {
-                throw new IOException(conn.getResponseCode() + " :: " + conn.getResponseMessage());
-            }
-        } finally {
-            if (conn != null)
-                conn.disconnect();
-        }
-    }
-
-    public static String getRemoteChars(String path, String charset) throws IOException {
-        httpVerify(path);
         CloseableHttpClient client = null;
         try {
             client = CommonHttpAPI.BUILD_DEFAULT_CLIENT();
 
-            HttpResponse response = CommonHttpAPI.doGet(client, path, null, null);
+            HttpResponseHolder response = CommonHttpAPI.doGetWithHolder(client, path, null, null);
+            if (!response.isResolveSuccess())
+                throw response.getResolveException();
 
-            int status = CommonHttpAPI.getStatusCode(response);
+            int status = response.getHttpStatusCode();
             if (200 == status) {
-                return CommonHttpAPI.getText(response, Charset.forName(charset));
+                return response.getResponseEntity();
             } else {
-                String mes = CommonHttpAPI.getReasonPhrase(response);
+                String mes = response.getReasonPhrase();
                 throw new IOException(status + " :: " + mes);
             }
         } catch (URISyntaxException e) {
             throw new IOException(e);
+        } catch (Throwable throwable) {
+            if (throwable instanceof IOException)
+                throw (IOException) throwable;
+            else
+                throw new IOException(throwable);
         } finally {
-            CommonHttpAPI.closeClient(client);
+            CommonHttpAPI.close(client);
         }
     }
+
+    public static String getRemoteChars(String path, String charset) throws IOException {
+        return new String(getRemoteBytes(path), charset);
+    }
+
+    public static String getRemoteChars(String path) throws IOException {
+        return new String(getRemoteBytes(path), CommonHttpAPI.getThreadEncoding());
+    }
+
 }
