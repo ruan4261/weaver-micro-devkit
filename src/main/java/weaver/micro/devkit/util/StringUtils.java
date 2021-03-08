92b78646c6c3afa5f2107cc240325324d6c3da46
ruan4261 <i@4261.ink>
2021-03-08 18:10:56 +0800

    :sparkles: feature(1.1.1): StringUtils新增方法, 校验目标数值是否在字符串中

--- src/main/java/weaver/micro/devkit/util/StringUtils.java
+++ src/main/java/weaver/micro/devkit/util/StringUtils.java
@@ -19,4 +19,45 @@ public class StringUtils {
         return builder.toString();
     }
 
+    /**
+     * Check whether s1 include s2(val.toString())
+     */
+    public static boolean isInclude(String s1, int val) {
+        Assert.notEmpty(s1);
+        int offset = 0;
+        int len = s1.length();
+        String s2 = Integer.toString(val);
+        int wid = s2.length();
+        int lastPossible = len - wid;
+
+        while (offset <= lastPossible) {
+            offset = s1.indexOf(s2, offset);
+            if (offset == -1)
+                return false;
+
+            int end = offset + wid;
+            // check start
+            if (offset > 0) {
+                char prev = s1.charAt(offset - 1);
+                if (prev == '-' || (prev >= '0' && prev <= '9')) {
+                    offset = end;
+                    continue;
+                }
+            }
+
+            // check end
+            if (end < len) {
+                char next = s1.charAt(end);
+                if (next >= '0' && next <= '9') {
+                    offset = end;
+                    continue;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
 }
