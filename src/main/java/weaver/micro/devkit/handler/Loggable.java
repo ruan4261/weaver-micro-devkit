c0be5509387ecd2b6dcd9bfbdaff943d54a7bc78
ruan4261 <i@4261.ink>
2021-03-16 16:15:36 +0800

    perf(1.1.4): LogEventProcessor性能优化

--- src/main/java/weaver/micro/devkit/handler/Loggable.java
+++ src/main/java/weaver/micro/devkit/handler/Loggable.java
@@ -5,10 +5,19 @@ package weaver.micro.devkit.handler;
  */
 public interface Loggable {
 
+    /**
+     * @since 1.1.3
+     */
     void log(String mes);
 
+    /**
+     * @since 1.1.3
+     */
     void log(Throwable throwable);
 
+    /**
+     * @since 1.1.3
+     */
     void log(String title, Throwable throwable);
 
 }
