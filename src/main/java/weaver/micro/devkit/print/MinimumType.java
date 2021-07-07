6cf71a28d5599cacbb21cb08321ea91a82afbe17
ruan4261 <i@4261.ink>
2021-07-07 21:34:57 +0800

    :construction: progress(1.1.13): 为即将上线的feature提供支撑方法 1) MinimumType新增参数, 可使用自定义方法输出打印对象 2) MinimumType可对类成员字段进行注解(如类注解和字段注解同时存在且参数不同, 字段注解生效)

--- src/main/java/weaver/micro/devkit/print/MinimumType.java
+++ src/main/java/weaver/micro/devkit/print/MinimumType.java
@@ -3,10 +3,33 @@ package weaver.micro.devkit.print;
 import java.lang.annotation.*;
 
 /**
- * Print objects using the toString() function.
+ * The priority of Field takes precedence over the priority of Type.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(value = {ElementType.TYPE})
+@Target(value = {ElementType.TYPE, ElementType.FIELD})
 public @interface MinimumType {
+
+    Class<?> serializationClass() default Object.class;
+
+    /**
+     * Based on {@link #serializationClass()}, the returned value
+     * will be call the method {@link Object#toString()},
+     * if it is null, output 'null'.
+     */
+    String serializationMethod() default "toString";
+
+    /**
+     * The parameters list of {@link #serializationMethod()}.
+     */
+    Class<?>[] parametersList() default {};
+
+    /**
+     * It represents the position of the print object in the parameter list,
+     * it starts at 1, and if it is 0, the printed object will be called,
+     * it cannot be greater than the length of {@link #parametersList()}.
+     * Others parameter position will be filled with null.
+     */
+    int callIndex() default 0;
+
 }
