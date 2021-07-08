f1ed639eba5cd6dc1de758260a7d24d2796efc8e
ruan4261 <i@4261.ink>
2021-07-08 16:47:51 +0800

    :sparkles: feature(1.1.13): 新增NotNull注解, 仅用于语义提醒

---
+++ src/main/java/weaver/micro/devkit/annotation/NotNull.java
@@ -0,0 +1,9 @@
+package weaver.micro.devkit.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
+public @interface NotNull {
+}
