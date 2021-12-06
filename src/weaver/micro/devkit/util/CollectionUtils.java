a93169edbd84e072e7ee17800d7e7b2f0cc50267
ruan4261 <i@4261.ink>
2021-12-06 19:36:49 +0800

    :sparkles: feature(2.1.0): 增加snakeCase向camelCase转换的方法(供键为String类型的Map使用)

--- src/weaver/micro/devkit/util/CollectionUtils.java
+++ src/weaver/micro/devkit/util/CollectionUtils.java
@@ -4,9 +4,7 @@ import weaver.conn.RecordSet;
 import weaver.micro.devkit.Assert;
 import weaver.micro.devkit.handler.StrictRecordSet;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @since 2.0.1
@@ -17,6 +15,45 @@ public class CollectionUtils {
         V val = m.get(ori);
         m.remove(ori);
         m.put(dest, val);
+    }
+
+    /**
+     * <pre>
+     * 请注意
+     * 如果字符串的最后一个字符为 '_', 该字符会被直接删除, 不进行其他操作
+     * 如以下两个字符串经过处理后, 会变为同一个字符串, 导致映射被覆盖
+     * 'hello_world' >> 'helloWorld'
+     * 'hello_world_' >> 'helloWorld'
+     * </pre>
+     */
+    public static <V> void snakeCase2CamelCase(Map<String, V> m) {
+        Map<String, V> camelCaseReplace = new HashMap<String, V>(m.size());
+        Iterator<Map.Entry<String, V>> it = m.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, V> entry = it.next();
+            String k = entry.getKey();
+            V v = entry.getValue();
+
+            if (k != null && k.contains("_")) {
+                StringBuilder builder = new StringBuilder(k);
+                for (int i = 0; i < builder.length(); ) {
+                    int idx = builder.indexOf("_", i);
+                    if (idx < 0)
+                        break;
+
+                    builder.deleteCharAt(idx);
+
+                    if (idx < builder.length())
+                        builder.setCharAt(idx, Character.toUpperCase(builder.charAt(idx)));
+
+                    i = idx;// 此处不用 +1, 因为 builder 中的该下标已被动态移除
+                }
+
+                it.remove();
+                camelCaseReplace.put(builder.toString(), v);
+            }
+        }
+        m.putAll(camelCaseReplace);
     }
 
     @SuppressWarnings("unchecked")
